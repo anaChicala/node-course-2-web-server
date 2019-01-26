e4ad051d9afb42abb0154952f45469fa63f75feb
anaChicala <achicala620@alumnos.iua.edu.ar>
2019-01-26 17:47:19 -0300

    otro setup con el profile

--- server.js
+++ server.js
@@ -2,34 +2,49 @@ const express =  require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
-var app = express();
 const port = process.env.PORT || 3000;
+var app = express();
+
+
+
+hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine','hbs');
-app.use(express.static(__dirname + '/public' ));
+
 
 app.use((req,res,next) => {
   let now = new Date().toString();
-
-
   console.log(now);
   next();
-
-
-
 });
+
+app.use(express.static(__dirname + '/public' ));
 
 app.get('/',(req,res)=>{
 
   res.render('home.hbs',{
-    welcome: 'Bienvenidos a mi pagina',
+    title: 'Welcome pagina',
     anio:2018
 
   });
 
 });
+app.get('/profile',(req,res)=>{
+
+  res.render('profile.hbs',{
+    title: 'Profile pagina',
+    anio:2018
+
+  });
+
+});
+
 app.get('/about',(req,res)=>{
 
-  res.render('about.hbs');
+  res.render('about.hbs',{
+    title: 'Help pagina',
+    anio:2018
+
+  });
 
 });
 
